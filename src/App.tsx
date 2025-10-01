968cfa199f64f7004b3eeb4fec952d0c6f9a5a82
MongkauFryan <12410420@student.unklab.ac.id>
2025-10-01 11:20:37 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { PengenalanJSX } from './course/PengenalanJSX'
-import SingleRootElement from './course/SingleRootElement'
-import DisplayData from './course/DisplayData'
-import FunctionalComponent from './course/FunctionalComponent'
-import PropsComponent from './course/PropsComponent'
+import React from 'react';
+import './App.css';
+import './w3.css';
+import PropsComponent from './course/cardcomponent';
 
 function App() {
   return (
-    <>
-      <DisplayData />
-      <PengenalanJSX />
-      <SingleRootElement />
-      <FunctionalComponent />
-      <PropsComponent
-      name='John Doe'
-      age={30}
-      isStudent={false} />
-    </>
+    <div className="w3-container w3-center">
+      {/* Tambahkan judul showroom di sini */}
+      <h1 className="w3-padding w3-xlarge">Showroom Fryxman</h1>
+      <hr />
+      <div style={{ display: 'flex', gap: '20px', justifyContent: 'center', padding: '20px', flexWrap: 'wrap' }}>
+        <PropsComponent
+          carName="Tesla Model S"
+          imageSrc="https://images.unsplash.com/photo-1617788138017-80ad40651399?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          year={2023}
+          isElectric={true}
+        />
+        <PropsComponent
+          carName="Ford Mustang"
+          imageSrc="https://images.unsplash.com/photo-1602200059552-39ed78989991?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          year={2022}
+          isElectric={false}
+        />
+        <PropsComponent 
+          carName="Honda Civic Type R"
+          imageSrc="https://c4.wallpaperflare.com/wallpaper/677/198/368/honda-civic-type-r-honda-civic-fk-honda-civic-fk8-honda-mugen-jdm-hd-wallpaper-thumb.jpg"
+          year={2024}
+          isElectric={false}
+        />
+        <PropsComponent
+          carName="Nissan Leaf"
+          imageSrc="https://www.blackhawkjapan.com/cdn/shop/products/KWAM002186_01_800x.jpg?v=1558090156"
+          year={2023}
+          isElectric={true}
+        />
+        <PropsComponent
+          carName="Chevrolet Corvette"
+          imageSrc="https://images.unsplash.com/photo-1612071078206-bc912b559e3a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          year={2024}
+          isElectric={false}
+        />
+      </div>
+    </div>
   );
 }
 
-export default App
+export default App;
